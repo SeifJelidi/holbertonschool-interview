0fd8ebb84a9914ddf70521527814aaae8cdb03ba
Seif Jelidi <seifjelidi3@gmail.com>
2021-01-07 21:38:47 +0000

    fixed

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -4,7 +4,9 @@ calculates the fewest number of operations
 needed to result in exactly n H characters in the file
 '''
 
+
 def minOperations(n):
+
     if n <= 1:
         return 0
 
@@ -16,4 +18,3 @@ def minOperations(n):
             n /= op
         op += 1
     return nb
-        
