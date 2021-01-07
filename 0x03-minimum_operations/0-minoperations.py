1764c892d9162a30cfffefa2ca087feb1299713c
Seif Jelidi <seifjelidi3@gmail.com>
2021-01-07 21:35:33 +0000

    task0

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+calculates the fewest number of operations
+needed to result in exactly n H characters in the file
+'''
+
+def minOperations(n):
+    if n <= 1:
+        return 0
+
+    op = 2
+    nb = 0
+    while op <= n:
+        while n % op == 0:
+            nb += op
+            n /= op
+        op += 1
+    return nb
+        
