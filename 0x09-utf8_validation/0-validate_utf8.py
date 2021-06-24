c7cae72dd6d8d3b6fade96520b56a5253659dc0f
Seif Jelidi <seifjelidi3@gmail.com>
2021-06-24 22:18:51 +0000

    try to fix

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -5,6 +5,6 @@
 def validUTF8(data):
     try:
         bytes(data).decode(encoding="UTF-8", errors="strict")
-            return True
+        return True
     except Exception:
         return False
