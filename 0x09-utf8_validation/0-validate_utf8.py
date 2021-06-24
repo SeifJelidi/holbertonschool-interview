4f396aa593b4e695fcd87e6ff523b8af634c22d8
Seif Jelidi <seifjelidi3@gmail.com>
2021-06-24 21:45:16 +0000

    fix pycode style

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -4,8 +4,8 @@ import codecs
 
 
 def validUTF8(data):
-   try:
-        s = bytes(data).decode(encoding ="UTF-8", errors="strict")
+    try:
+        s = bytes(data).decode(encoding="UTF-8", errors="strict")
         return True
-   except Exception:
-       return False
+    except Exception:
+        return False
