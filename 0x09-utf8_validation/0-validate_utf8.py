6e3f719a4f095b36468866ce1f869091f6065751
Seif Jelidi <seifjelidi3@gmail.com>
2021-06-24 20:45:36 +0000

    utf validation

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""UTF 8 validaion"""
+import codecs
+
+def validUTF8(data):
+    try:
+       bytes(data).decode("UTF-8", errors="strict")
+       return True
+    except Exception:
+        return False
