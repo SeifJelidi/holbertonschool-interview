9de624bebce139bb43d1224e4ff7789d1983da48
Your Name <you@example.com>
2021-10-28 23:17:52 +0100

    fix

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -7,7 +7,7 @@ determine the fewest number of coins needed to meet a given amount total.
 def makeChange(coins, total):
     """determine the fewest number of coins needed to meet a given amount total.
     """
-    if total <= 1:
+    if total <= 0:
         return 0
     coins.sort()
     coins.reverse()
